0f2c8caab3483a1ef3993fbaf42470ba6db3611c
Ana María García Sánchez <anamariagarciasanchez@MacBook-Pro-de-Ana.local>
2021-02-23 10:07:33 +0100

    Trying to add elasticsearch

--- src/main/java/controllers/HelloContoller.java
+++ src/main/java/controllers/HelloContoller.java
@@ -3,12 +3,22 @@ package controllers;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 
 @Controller("/hello")
 public class HelloContoller {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index(){
+
+        //RestHighLevelClient client = new RestHighLevelClient(
+        //       RestClient.builder(
+        //                new HttpHost("localhost", 9200, "http"),
+        //                new HttpHost("localhost", 9201, "http")));
+        //client.close();
+
         return "Hello World";
     }
 
